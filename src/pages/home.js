c3a7415c70a6dfe2a8d0a5e4ee6c649dcc4219db
Daniel Roman <dan.roman21@gmail.com>
2020-08-24 16:22:33 -0400

    Started applying card formatting to screams

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,20 +7,22 @@ import Scream from '../components/Scream';
 class home extends Component {
     state = {
         screams: null
-    }
+    };
     componentDidMount(){
-        axios.get('/screams')
-            .then(res => {
-                console.log(res.data)
+        axios
+            .get('/screams')
+            .then((res) => {
                 this.setState({
                     screams: res.data
-                })
+                });
             })
-            .catch(err => console.log(err));
+            .catch((err) => console.log(err));
     }
     render(){
         let recentScreamsMarkup = this.state.screams ? (
-            this.state.screams.map(scream => <Scream scream={scream}/>)
+            this.state.screams.map((scream) => (
+                <Scream key={scream.screamId} scream={scream}/>
+            ))
         ) : (
             <p>Loading...</p>
         );
@@ -30,7 +32,6 @@ class home extends Component {
                     {recentScreamsMarkup}
                 </Grid>
                 <Grid item sm={4} xs={12}>
-                    <p>Profile...</p>
                 </Grid>
             </Grid>
         );
